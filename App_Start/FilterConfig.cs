d054b8c516e16c4c960741feb4b4b867afe365d4
RobertoTajamar <37329405+RobertoTajamar@users.noreply.github.com>
2018-04-27 13:51:57 +0200

    Add files via upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FirscodeProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
